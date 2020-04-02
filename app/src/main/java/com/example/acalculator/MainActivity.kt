b7c702d3ddbe46940eaf6f74c1e84e7c1658ad16
luis <a21702482@alunos.ulht,pt>
2020-04-02 13:31:10 +0100

    Correção lists

--- app/src/main/java/com/example/acalculator/MainActivity.kt
+++ app/src/main/java/com/example/acalculator/MainActivity.kt
@@ -37,52 +37,52 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i(TAG,"o metodo onCreate foi invocado")
+        Log.i(TAG, "o metodo onCreate foi invocado")
         setContentView(R.layout.activity_main)
-        list_historic?.adapter = HistoryAdapter(this, R.layout.item_expression, lista)
-
-
-    }
-    fun onClickHistorico(view: View) {
-        startActivity(Intent(this,HistoryActivity::class.java))
-        finish()
-    }
-    fun onClickDeleteAll(view: View) {
-        Log.i(TAG, "Click no botão C")
-        text_visor.setText("0")
-    }
-    fun onClickSymbol(view: View) {
-        val symbol = view.tag.toString()
-        Log.i(TAG, "Click no botão ${symbol}")
-        if (text_visor.text.toString() == "0"){
-            text_visor.text = symbol
-        } else {
-            text_visor.append(symbol)
-        }
-    }
-    fun onClickDelLastCharacter(view: View){
-        Log.i(TAG, "Click no botão <")
-        if (text_visor.length() > 1) {
-            val texto = text_visor.text.substring(0, text_visor.length() - 1)
-            text_visor.text = texto.toString()
-        } else {
-            text_visor.setText("0")
-        }
-    }
-    fun onClickEquals(view: View) {
-        Log.i(TAG, "Click no botão =")
-       // Operation operation
-        var expressao = text_visor.text.toString()
-        ultimo = text_visor.text.toString()
-        val expression = ExpressionBuilder(text_visor.text.toString()).build()
-        text_visor.text = expression.evaluate().toString()
-        Log.i(TAG, "O resutlado da expressão é ${text_visor.text}")
-        var resultado = text_visor.text.toString()
-        lista.add(Operation(expressao,resultado.toDouble()))
         list_historic?.layoutManager = LinearLayoutManager(this)
         list_historic?.adapter = HistoryAdapter(this, R.layout.item_expression, lista)
-
     }
+
+        fun onClickHistorico(view: View) {
+            startActivity(Intent(this, HistoryActivity::class.java))
+            finish()
+        }
+        fun onClickDeleteAll(view: View) {
+            Log.i(TAG, "Click no botão C")
+            text_visor.setText("0")
+        }
+        fun onClickSymbol(view: View) {
+            val symbol = view.tag.toString()
+            Log.i(TAG, "Click no botão ${symbol}")
+            if (text_visor.text.toString() == "0") {
+                text_visor.text = symbol
+            } else {
+                text_visor.append(symbol)
+            }
+        }
+        fun onClickDelLastCharacter(view: View) {
+            Log.i(TAG, "Click no botão <")
+            if (text_visor.length() > 1) {
+                val texto = text_visor.text.substring(0, text_visor.length() - 1)
+                text_visor.text = texto.toString()
+            } else {
+                text_visor.setText("0")
+            }
+        }
+        fun onClickEquals(view: View) {
+            Log.i(TAG, "Click no botão =")
+            // Operation operation
+            var expressao = text_visor.text.toString()
+            ultimo = text_visor.text.toString()
+            val expression = ExpressionBuilder(text_visor.text.toString()).build()
+            text_visor.text = expression.evaluate().toString()
+            Log.i(TAG, "O resutlado da expressão é ${text_visor.text}")
+            var resultado = text_visor.text.toString()
+            lista.add(Operation(expressao, resultado.toDouble()))
+            list_historic?.layoutManager = LinearLayoutManager(this)
+            list_historic?.adapter = HistoryAdapter(this, R.layout.item_expression, lista)
+
+        }
 }
 class HistoryAdapter(private val context: Context, private val layout: Int, private val items: MutableList<Operation>) : RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
         class HistoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
